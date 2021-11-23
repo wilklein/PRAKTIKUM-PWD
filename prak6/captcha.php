147999ad56cac89b12732c00339552a1b6648362
wilklein <50920214+wilklein@users.noreply.github.com>
2021-11-23 20:50:42 +0700

    Update captcha.php

--- prak6/captcha.php
+++ prak6/captcha.php
@@ -8,5 +8,6 @@ $captcha_background = imagecolorallocate($target_layer, 255, 160, 119);
 imagefill($target_layer,0,0,$captcha_background);
 $captcha_text_color = imagecolorallocate($target_layer, 0, 0, 0);
 imagestring($target_layer, 5, 5, 5, $captcha_code, $captcha_text_color);
-header("content-type: image/jpeg"); imagejpeg($target_layer);
+header("content-type: image/jpeg"); 
+imagejpeg($target_layer);
 ?>
