da11fcc2f09f4614b38c0cf9dd9ba19326264b46
wilklein <50920214+wilklein@users.noreply.github.com>
2021-11-23 20:28:12 +0700

    Add files via upload

---
+++ prak6/form_login.php
@@ -0,0 +1,13 @@
+<h1>login</h1>
+<form method=post action=cek_login.php>
+<table>
+<tr><td>Username</td><td> : <input name='id_user' type='text'></td></tr>
+<tr><td>Password</td><td> : <input name='paswd' type='text'></td></tr>
+<tr>
+<td>Captcha<br>
+<img src="captcha.php"/></td>
+<td> : <input name='captcha_code' type='text'></td></tr>
+<tr><td colspan=2><input type='submit' value='LOGIN'></td></tr>
+</table>
+</form>
+
